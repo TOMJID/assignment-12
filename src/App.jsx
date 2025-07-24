694c0d08d5045df14522b54409a1598fbe0cb940
Tomjid huda <94817887+TOMJID@users.noreply.github.com>
2025-07-24 14:20:19 +0600

    setup complete

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@heroui/button";
+
+function App() {
+  return (
+    <>
+      <h1>hello world! </h1>
+      <Button color="danger" size="lg">
+        click me !
+      </Button>
+    </>
+  );
+}
+
+export default App;
